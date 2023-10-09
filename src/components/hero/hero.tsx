09dd7d479e460db3e3f635171b2f7628ed4c652f
Chanda Abdul <56906759+Chanda-Abdul@users.noreply.github.com>
2023-10-08 17:20:10 -0700

    gsap desktop hero anims

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,5 +1,6 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useVisibleTask$ } from '@builder.io/qwik';
 import styles from './hero.module.scss';
+
 import ImgBlur from '~/media/blur.jpeg?jsx';
 import ImgKiki from '~/media/kiki.jpeg?jsx';
 import ImgLacingUp from '~/media/lacingup.jpeg?jsx';
@@ -8,35 +9,64 @@ import ImgSunglasses from '~/media/sunglasses.jpeg?jsx';
 import ImgThousandYardStare from '~/media/thousandyardstare.jpeg?jsx';
 import ImgTrioTrailRunning from '~/media/triotrailrunning.jpeg?jsx';
 
+import gsap from 'gsap';
+
 export default component$(() => {
-  // TO-DO => hide images and text
+  
+  useVisibleTask$(() => {
+    // Only runs in the client, for GSAP animations
+    const tl = gsap.timeline({
+      defaults: { autoAlpha: 0, ease: 'expo.out' },
+    });
+
+    function initHeroTimeline() {
+   
+      tl.from('#first', {
+        x: '-150',
+        duration: 2,
+      })
+        .from(
+          '#last',
+          {
+            x: '150',
+            autoAlpha: 0,
+            duration: 1.75,
+          },
+          '-=1.25'
+        )
+        .from(
+          // TO-DO => parallex w/ScrollTrigger
+          '#gallery > *',
+          {
+            transformOrigin: '50% 50%',
+            // x: gsap.utils.random(50, 100),
+            // y: gsap.utils.random(100, 50),
+            scale: 0.25,
+            stagger: 0.04,
+            ease: 'power3.out',
+            duration: 1.5,
+          },
+          '-=0.75'
+        );
+    }
+
+    initHeroTimeline();
+  });
+
   return (
     <div class={['container', styles.hero]}>
-
       <section class={styles['hero']}>
+        <h1 class={styles['hero__title']} id='intro'>
+          <span class={styles['hero__title--first']} id='first'>
+            Outdoor
+          </span>
 
-        <h1 class={styles['hero__title']} >
-
-          <div class={styles['hero__title--first']}>
-            <span>O</span>
-            <span>u</span>
-            <span>t</span>
-            <span>d</span>
-            <span>o</span>
-            <span>o</span>
-            <span>r</span>
-          </div>
-
-          <div class='hero__title--last'>
-            <span>T</span>
-            <span>o</span>
-            <span>n</span>
-            <span>e</span>
-            <span>s</span>
-          </div>
+          <span class={styles['hero__title--last']} id='last'>
+            Tones
+          </span>
         </h1>
 
-        <div class={styles['hero__gallery']}>
+        <div class={styles['hero__gallery']} id='gallery'>
           <ImgSunglasses />
           <ImgTrioTrailRunning />
           <ImgLacingUp />
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,41 +1,16 @@
-import { component$ } from '@builder.io/qwik';
-import styles from './home.module.scss';
+import { component$} from '@builder.io/qwik';
 
 import Hero from '../hero/hero';
 import About from '../about/about';
 import CallToAction from '../call-to-action/call-to-action';
-// import gsap from 'gsap';
-// import Lenis from '@studio-freight/lenis';
-// import { time } from 'console';
+
 
 export default component$(() => {
-
-//     class App {
-//     // TO-DO => add lenis for smooth scrolling https://github.com/studio-freight/lenis
-
-//     // lenis = new Lenis();
-//     constructor() {
-//       this._initialize();
-//       //   this._render(time!);
-//     }
-
-//     _initialize() {
-//       // TO-DO => initial()
-//       this._setInitialState();
-//       this._createLenis();
-//       this._createIntro();
-//       this._createHero();
-//     }
+// TO-DO => add lenis for smooth scrolling https://github.com/studio-freight/lenis
 
 //     _setInitialState() {
-//       // TO-DO => hide images and text
 //       gsap.set(['.hero__title span', '.about__list--value p', '.banner'], {
 //         y: 32,
-//         opacity: 0,
-//       });
-
-//       gsap.set('.hero__gallery', {
-//         y: gsap.utils.random(100, 50),
 //         opacity: 0,
 //       });
 
@@ -44,54 +19,8 @@ export default component$(() => {
 //       });
 //     }
 
-//     _createLenis() {
-//       //   this.lenis = new Lenis({
-//       //     lerp: 0.1,
-//       //   });
-//     }
-//     _createIntro() {
-//       // TO-DO =>
-//       const tl = gsap.timeline();
-//       tl.to('.hero__title', {
-//         opacity: 1,
-//       })
-//         .to('hero__title span', {
-//           y: 0,
-//           ease: 'expo.out',
-//           duration: 2,
-//           stagger: 0.01,
-//         })
-//         .to(
-//           '.hero__gallery img',
-//           {
-//             opacity: 1,
-//             y: 0,
-//             ease: 'power3.out',
-//             duration: 2,
-//             stagger: 0.04,
-//           },
-//           0.5
-//           // run 0.5s after tl has started or
-//           // -=1s after timeline starts
-//         );
-//     }
-
-//     _createHero() {
-//       // TO-DO => parallex w/ScrollTrigger
-//       const tl = gsap.timeline();
-//     }
-
-//     _render() {
-//       // TO-DO => render() lenis
-//       //   this.lenis.raf(time);
-//       //   requestAnimationFrame(this._render.bind(this));
-//     }
-//   }
-
-//   new App();
-
   return (
-    <div class={['container', styles.home]}>
+    <div class={['home']}>
       <Hero />
 
       <About />
